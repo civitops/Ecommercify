cc84e8f7039e7da694af61267655da7525c9e838
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-21 05:53:25 +0000

    HTTP endpoints done

--- user/implementation/user/entity.go
+++ user/implementation/user/entity.go
@@ -8,7 +8,7 @@ type Entity struct {
 	Name            string   `json:"name" gorm:"not null"`
 	PhoneNo         string   `json:"phoneNo"  gorm:"unique"`
 	Email           string   `json:"email" gorm:"unique"`
-	Password        string   `json:"password" gorm:"unique"`
+	Password        string   `json:"password"`
 	HomeAddress     Address  `json:"homeAddress"  gorm:"embedded;embeddedPrefix:homeaddress_"`
 	DeliveryAddress Address  `json:"deliveryAddress"  gorm:"embedded;embeddedPrefix:deliveryaddress_"`
 	Role            RoleType `json:"role" gorm:"default:4;"`
--- user/transport/grpc/server.go
+++ user/transport/grpc/server.go
@@ -7,9 +7,11 @@ import (
 	userPb "github.com/civitops/Ecommercify/user/proto/user"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func newGrpcServer(l *zap.SugaredLogger, u user.Service, t trace.Tracer) *server {
+func NewGrpcServer(l *zap.SugaredLogger, u user.Service, t trace.Tracer) *server {
 	return &server{
 		log:   l,
 		user:  u,
@@ -27,6 +29,7 @@ type server struct {
 func (s *server) CreateUser(ctx context.Context, u *userPb.User) (*userPb.UserId, error) {
 	ctxSpan, span := s.trace.Start(ctx, "create-user-grpc")
 	defer span.End()
+	//TODO: Check conditions
 	usr := user.Entity{
 		Name:     u.Name,
 		PhoneNo:  u.PhoneNo,
@@ -50,9 +53,24 @@ func (s *server) CreateUser(ctx context.Context, u *userPb.User) (*userPb.UserId
 	}
 	id, err := s.user.Create(ctxSpan, usr)
 	userId := userPb.UserId{Id: uint32(id)}
-	return &userId, err
+	return &userId, status.Errorf(codes.Internal, err.Error())
 }
 
-// func (s *server) GetUserByID(ctx context.Context, u *userPb.SignInUser) (*userPb.SignInAck, error) {
-
-// }
+func (s *server) GetUserByID(ctx context.Context, u *userPb.SignInUser) (*userPb.SignInAck, error) {
+	ctxSpan, span := s.trace.Start(ctx, "get-user-grpc")
+	defer span.End()
+	where := make(map[string]interface{})
+	if u.Email == "" && u.PhoneNo != "" {
+		where["phoneNo"] = u.PhoneNo
+	} else if u.Email != "" && u.PhoneNo == "" {
+		where["email"] = u.Email
+	} else {
+		return nil, status.Errorf(codes.InvalidArgument, "email or phoneNo must be provided")
+	}
+	where["role"] = u.RoleType
+	user, err := s.user.Get(ctxSpan, "password", where)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	return &userPb.SignInAck{IsValid: true, Password: user.Password}, nil
+}
